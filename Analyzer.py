2f6e79d62a2b09dffa21bbec0a997d2d01c03987
MolinWu <willlin0022@gmail.com>
2025-06-14 10:36:26 +0800

    update

--- Analyzer.py
+++ Analyzer.py
@@ -185,9 +185,10 @@ class statistic():
         lst.sort(key=lambda x: x[-1], reverse=True)
         with open(f"results_{function.__name__}.txt", "w+") as f:
             for (name, op, line, col), val in lst:
-                if name == 'None' or name in exist: # or op != 1 or name in exist: # EXP_OP.SIG = 1
+                if name in exist: # or op != 1 or name in exist: # EXP_OP.SIG = 1
                     continue
-                exist.append(name)
+                if not name == 'None':
+                    exist.append(name)
                 print(f"\t{name}:\tline:{line}, col{col}: \t{val:.4f}") 
                 f.write(f"\t{name}:\tline:{line}, col{col}: \t{val:.4f}\n") 
                 cnt += 1
