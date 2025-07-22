5a95cd8e51afcb134d6d51214e92f7f79a65e6dd
MolinWu <willlin0022@gmail.com>
2025-07-22 18:26:37 +0800

    fix

--- tools.py
+++ tools.py
@@ -284,7 +284,7 @@ class Fault_Locate_Analyzer_2(Fault_Locate_Analyzer):
         self.sorted_var_lists = sorted(located_var_lists, key=lambda x: x[1], reverse=True)
         ret = 0.0
         try:
-            ret = max(self.dist_factor(w/max(w for (s, w) in self.sorted_var_lists[:self.vars_limit]), abs(self.dists[s][self.faulty_var])) if s in self.dists and self.faulty_var in self.dists[s] and self.dists[s][self.faulty_var] != float('inf') else 0 for (s, w) in self.sorted_var_lists[:self.vars_limit])
+            ret = max(self.dist_factor(w/max(w for (s, w) in self.sorted_var_lists[:self.vars_limit]), abs(self.dists[self.faulty_var][s])) if self.faulty_var in self.dists and s in self.dists[self.faulty_var] and self.dists[self.faulty_var][s] != float('inf') else 0 for (s, w) in self.sorted_var_lists[:self.vars_limit])
         except Exception as e:
             with open("./error.log", "w") as f:
                 f.write(f"Error: faulty_var: {faulty_var}. Skipping\n")
